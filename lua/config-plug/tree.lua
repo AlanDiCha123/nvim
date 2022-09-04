45bc542e9b977f361e45f06c90a1ff8c53d69445
AlanDiCha123 <didier.ominguez@gmail.com>
2022-09-04 17:45:56 -0500

    Fixing the breakable plugins

--- lua/config-plug/tree.lua
+++ lua/config-plug/tree.lua
@@ -1,16 +1,12 @@
 require'nvim-tree'.setup {
   disable_netrw       = true,
   hijack_netrw        = true,
-  open_on_setup       = false,
+--  open_on_setup       = false,
   ignore_ft_on_setup  = {},
-  auto_close          = false,
+--  auto_close          = false,
   open_on_tab         = false,
   hijack_cursor       = false,
   update_cwd          = false,
-  update_to_buf_dir   = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = false,
     icons = {
@@ -43,7 +39,6 @@ require'nvim-tree'.setup {
     height = 30,
     hide_root_folder = false,
     side = 'left',
-    auto_resize = false,
     mappings = {
       custom_only = false,
       list = {}
